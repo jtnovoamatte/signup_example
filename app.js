386bc548e8f2fc9b4e7879c5c2bc1b3ae6e84c93
José Tomás <111134022+jtnovoamatte@users.noreply.github.com>
2022-10-03 16:11:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -46,10 +46,6 @@ app.post("/", function (req, res) {
     } else {
       res.sendFile(__dirname + "/failure.html");
     }
-
-    // response.on("data", function (data) {
-    //   console.log(JSON.parse(data));
-    // });
   });
 
   request.end();
