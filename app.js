7add093e290b2a798241f315d67b1e2af07bdc67
José Tomás <jtnovoamatte@gmail.com>
2022-10-03 15:42:56 -0300

    Heroku installed OK by JT

--- app.js
+++ app.js
@@ -41,9 +41,11 @@ app.post("/", function (req, res) {
   };
 
   const request = https.request(url, options, function (response) {
-    response.statusCode == 200
-      ? res.sendFile(__dirname + "/success.html")
-      : res.sendFile(__dirname + "/failure.html");
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
 
     // response.on("data", function (data) {
     //   console.log(JSON.parse(data));
@@ -51,6 +53,10 @@ app.post("/", function (req, res) {
   });
 
   request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 4000, function () {
